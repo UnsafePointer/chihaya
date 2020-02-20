4a6e205c1228243bced05da4889c205e99a6bbed
Renzo Crisostomo <renzo@crisostomo.me>
2020-02-20 20:31:32 +0100

    Implement ROM file read

--- src/main/com/unsafepointer/chihaya/core.clj
+++ src/main/com/unsafepointer/chihaya/core.clj
@@ -1,5 +1,12 @@
 (ns com.unsafepointer.chihaya.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io])
+  (:require [com.unsafepointer.chihaya.emulator :as emulator]))
 
 (defn -main [& args]
-  (println "Hello, world!"))
+  (let [file-path (or (first args) "")
+        file-exists (.exists (io/file file-path))
+        file-not-directory (not (.isDirectory (io/file file-path)))]
+    (if (and file-exists file-not-directory)
+      (prinln "All good!")
+      (println "Incorrect argument passed. See README.md for usage."))))
