fe16971f65136ed2655f163cd706ffccb0165d55
Renzo Crisostomo <renzo@crisostomo.me>
2020-02-22 12:05:39 +0100

    Implement instruction fetch

--- src/main/com/unsafepointer/chihaya/emulator.clj
+++ src/main/com/unsafepointer/chihaya/emulator.clj
@@ -5,11 +5,32 @@
 (defn read-rom [file-path]
   (IOUtils/toByteArray (io/input-stream file-path)))
 
+(defn create-initial-state [file-path]
+  (let [bytes (read-rom file-path)
+        rom (map #(Byte/toUnsignedInt %) bytes)
+        memory (vec (concat (vec (repeat 0x200 0)) ; 0x000 to 0x1FF is reserved for the interpreter
+                            (seq rom) ; 0x200 is the start of most Chip-8 programs
+                            (vec (repeat (- 0xFFF (count rom) 0x200) 0)))) ; 0xFFF is the end of Chip-8 RAM
+        state (atom {:memory memory
+                     :program-counter  0x200
+                     :current-instruction nil})]
+    state))
+
+(defn read-current-instruction [state]
+  (let [program-counter (:program-counter @state)
+        memory (:memory @state)
+        instruction (+ (bit-shift-left (nth memory program-counter) 8)
+                                       (nth memory (inc program-counter)))]
+    (swap! state assoc :current-instruction instruction)
+    (swap! state assoc :program-counter (+ program-counter 2)))) ; TODO: Check when to increase PC
+
 (defn execute-next-instruction [state]
+  (prn "Current instruction: " (:current-instruction @state))
+  (prn "Current program counter: " (:program-counter @state))
   (prn "Executing next instruction"))
 
 (defn start-emulation [file-path]
-  (let [rom (read-rom file-path)]
+  (let [state (create-initial-state file-path)]
     (while true
-      (->> {:ROM rom}
-        execute-next-instruction))))
+      (read-current-instruction state)
+      (execute-next-instruction state))))
